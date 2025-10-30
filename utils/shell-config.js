2533fe509c25b668f205912083eb17e0a24d06a2
Keith Miller <keith_miller@apple.com>
2025-10-30 10:53:38 +0100

    Add console.debug to the list

--- utils/shell-config.js
+++ utils/shell-config.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Apple Inc. All rights reserved.
+ * Copyright (C) 2018-2025 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,7 +27,7 @@ const isInBrowser = false;
 if (typeof console == "undefined")
     console = {};
 
-console.debug ??= (...args) => console.log("Debug:", ...args);
+console.debug ??= (...args) => printErr("Debug: " + args.join(" "));
 console.log ??= (...args) => print(args.join(" "));
 console.warn ??= (...args) => printErr("Warn: " + args.join(" "));
 console.error ??= (...args) => printErr("Error: " + args.join(" "));
